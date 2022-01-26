e07a864d00ae20359ef5a206fded5e5bacb4e9db
Gao Xiangkang <1148118271@qq.com>
2022-01-26 19:16:20 +0800

    remove openssl rust bindings because of build difficulties

--- src/encryption/rsa.rs
+++ src/encryption/rsa.rs
@@ -1,47 +1,39 @@
-use openssl::bn::BigNum;
-use openssl::hash::MessageDigest;
-use openssl::pkey::PKey;
-use openssl::rsa;
-use openssl::sign::Verifier;
-use crate::encryption::PublicKey;
+use rsa::PublicKey;
+use crate::encryption::PublicKey as PubK;
 use crate::error::SshErrorKind;
 use crate::packet::Data;
 use crate::SshError;
 
+fn u8s_to_u32s(v: Vec<u8>) -> Vec<u32> {
+    let mut vec = vec![];
+    for x in v {
+        vec.push(x as u32)
+    }
+    vec
+}
+
 pub(crate) struct RSA;
 
-impl PublicKey for RSA {
+impl PubK for RSA {
     fn new() -> Self where Self: Sized {
         Self
     }
 
     fn verify_signature(&self, ks: &[u8], message: &[u8], sig: &[u8]) -> Result<bool, SshError> {
+
         let mut data = Data((&ks[4..]).to_vec());
         data.get_u8s();
-        let e = match BigNum::from_slice(&(data.get_u8s())) {
-            Ok(e) => e,
-            Err(_) => return Err(SshError::from(SshErrorKind::SignatureError))
-        };
-        let n = match BigNum::from_slice(&(data.get_u8s())) {
-            Ok(n) => n,
-            Err(_) => return Err(SshError::from(SshErrorKind::SignatureError))
-        };
-        let puk = match rsa::Rsa::from_public_components(n, e) {
-            Ok(v) => v,
-            Err(_) => return Err(SshError::from(SshErrorKind::SignatureError))
-        };
-        let pkey = match PKey::from_rsa(puk) {
-            Ok(v) => v,
-            Err(_) => return Err(SshError::from(SshErrorKind::SignatureError))
-        };
-        let mut verifier = match Verifier::new(MessageDigest::sha1(), &pkey) {
-            Ok(v) => v,
-            Err(_) => return Err(SshError::from(SshErrorKind::SignatureError))
-        };
-        match verifier.verify_oneshot(sig, message) {
-            Ok(ok) => Ok(ok),
-            Err(_) => Ok(false)
-        }
 
+        let e = rsa::BigUint::from_bytes_be(data.get_u8s().as_slice());
+        let n = rsa::BigUint::from_bytes_be(data.get_u8s().as_slice());
+        let public_key = rsa::RsaPublicKey::new(n, e).unwrap();
+        let scheme = rsa::PaddingScheme::PKCS1v15Sign {
+            hash: Some(rsa::Hash::SHA1)
+        };
+
+        let digest = ring::digest::digest(&ring::digest::SHA1_FOR_LEGACY_USE_ONLY, message);
+        let msg = digest.as_ref();
+
+        Ok(public_key.verify(scheme, msg, sig).is_ok())
     }
 }
