893a6de886fc7d06093eafed25a1bf6e3989465e
Gao Xiangkang <1148118271@qq.com>
2022-05-22 17:16:04 +0800

    删除无用代码

--- src/global.rs
+++ src/global.rs
@@ -2,19 +2,14 @@ use std::collections::HashMap;
 use std::sync::atomic::AtomicU32;
 use std::sync::Mutex;
 use encryption::ChaCha20Poly1305;
-use crate::{Config, Client};
-// use crate::channel::ChannelWindowSize;
+use crate::Config;
 
 
 // 客户端通道编号初始值
 pub(crate) static CLIENT_CHANNEL: AtomicU32 = AtomicU32::new(0);
-
-// pub(crate) static mut CLIENT: Option<Mutex<Client>> = None;
 
 pub(crate) static mut CONFIG: Option<Mutex<Config>> = None;
 
 // 加密密钥
 pub(crate) static mut ENCRYPTION_KEY: Option<ChaCha20Poly1305> = None;
 
-// 每个通道的窗口大小
-// pub(crate) static mut CHANNEL_WINDOW: Option<HashMap<u32, Mutex<ChannelWindowSize>>> = None;
--- src/util.rs
+++ src/util.rs
@@ -70,44 +70,6 @@ pub(crate) fn update_encryption_key(v: Option<ChaCha20Poly1305>) {
     }
 }
 
-// pub(crate) fn get_channel_window(k: u32) -> SshResult<Option<MutexGuard<'static, ChannelWindowSize>>> {
-//     unsafe {
-//         if let None = CHANNEL_WINDOW {
-//             CHANNEL_WINDOW = Some(HashMap::new())
-//         }
-//
-//         if let Some(map) = &mut CHANNEL_WINDOW {
-//             return match map.get(&k) {
-//                 None => Ok(None),
-//                 Some(v) => {
-//                     match v.lock() {
-//                         Ok(v) => {
-//                             Ok(Some(v))
-//                         }
-//                         Err(e) => {
-//                             log::error!("get channel_window mutex error, error info: {:?}", e);
-//                             Err(SshError::from(SshErrorKind::MutexError))
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         Ok(None)
-//     }
-// }
-//
-// pub(crate) fn set_channel_window(k: u32, v: ChannelWindowSize) {
-//     unsafe {
-//         if let None = CHANNEL_WINDOW {
-//             CHANNEL_WINDOW = Some(HashMap::new())
-//         }
-//
-//         if let Some(map) = &mut CHANNEL_WINDOW {
-//             map.insert(k, Mutex::new(v));
-//         }
-//     }
-// }
-
 // 十六位随机数
 pub(crate) fn cookie() -> Vec<u8> {
     let cookie: [u8; 16] = OsRng.gen();
