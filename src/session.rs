1e29c0eafa2188089ffb8bcb641aec5c2782a834
1148118271 <1148118271@qq.com>
2022-07-08 14:33:29 +0800

    🎠public key signature

--- src/session.rs
+++ src/session.rs
@@ -252,6 +252,7 @@ impl Session {
                     }
                     ssh_msg_code::SSH_MSG_USERAUTH_PK_OK => {
                         log::info!("user auth support this algorithm.");
+                        self.public_key_signature()?
                         // return Ok(())
                     }
                     ssh_msg_code::SSH_MSG_USERAUTH_SUCCESS => {
@@ -337,5 +338,52 @@ impl Session {
         client.write(data)
     }
 
+    fn public_key_signature(&self) -> SshResult<()> {
+        let config = config::config();
+        let rprk = rsa::RsaPrivateKey::from_pkcs1_pem(config.auth.private_key.as_str()).unwrap();
+        let rpuk = rprk.to_public_key();
+        let es = rpuk.e().to_bytes_be();
+        let ns = rpuk.n().to_bytes_be();
+
+        let mut blob = Data::new();
+        blob.put_str("ssh-rsa");
+        blob.put_mpint(&es);
+        blob.put_mpint(&ns);
+        let blob = blob.as_slice();
+        println!("blob length : {}", blob.len());
+        let mut data = Data::new();
+        data.put_u8(ssh_msg_code::SSH_MSG_USERAUTH_REQUEST)
+            .put_str(config.auth.username.as_str())
+            .put_str(ssh_str::SSH_CONNECTION)
+            .put_str(ssh_str::PUBLIC_KEY)
+            .put_u8(true as u8)
+            .put_str(config.auth.private_key_algorithm_name.as_str())
+            .put_u8s(blob);
+
+        let session_id = h::get().digest();
+        let mut sd = Data::new();
+        sd.put_u8s(session_id.as_slice());
+        let s = data.clone();
+        sd.extend_from_slice(s.as_slice());
+        let scheme = rsa::PaddingScheme::PKCS1v15Sign {
+            hash: Some(rsa::Hash::SHA1)
+        };
+        println!("sd len {}", sd.len());
+        let digest = ring::digest::digest(&ring::digest::SHA1_FOR_LEGACY_USE_ONLY, sd.as_slice());
+        let msg = digest.as_ref();
+
+        //let vec = rprk.decrypt(scheme, msg).unwrap();
+        // rprk.decrypt()
+        // rprk.s
+        let sign = rprk.sign(scheme, msg).unwrap();
+        println!("sign length => {}", sign.len());
+        let mut ss = Data::new();
+        ss.put_str("ssh-rsa");
+        ss.put_u8s(sign.as_slice());
+        println!("ss len {}", ss.len());
+        data.put_u8s(ss.as_slice());
+        let client = client::default()?;
+        client.write(data)
+    }
 }
 
